c4cbcfa1acfbe7c71f34bb56d667e3a33d7ce7dd
Sakib Al Hasan <76560286+saifsaki@users.noreply.github.com>
2021-07-12 09:19:07 +0600

    Added Animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,14 @@ import About from "../components/home/about";
 import Skills from "../components/home/skills";
 import Bar from "../components/home/Bar";
 import Footer from "../components/Footer";
+import ParticleBackground from "../components/ParticleBackground";
 
 const Home = () => {
   return (
     <div className="App">
       <Header />
       <Hero />
+			<ParticleBackground />
       <About />
 			<Skills />
 			<Bar />
